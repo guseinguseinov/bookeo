0d7ffbda26b6813c47d969349c6e5456f62bd4fb
guseinguseinov <gusein.guseinov2002@gmail.com>
2022-07-27 01:27:48 +0100

    axir ki slideri elemeyin yolunu tapdim

---
+++ assets/js/swiper.js
@@ -0,0 +1,27 @@
+var flkty = new Flickity(document.querySelector('.main-gallery'), {
+    prevNextButtons: false,
+    autoPlay: 1500,
+    fade: true,
+
+});
+
+var swiper = new Swiper(".best-seller", {
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
+
+var swiper = new Swiper(".swiper2", {
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
+
+var swiper = new Swiper(".swiper3", {
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
