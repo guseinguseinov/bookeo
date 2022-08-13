d355c3318ac66f09660e7a87481370b4c9e4c77d
guseinguseinov <gusein.guseinov2002@gmail.com>
2022-08-13 17:05:42 +0100

    swiper done

--- assets/js/swiper.js
+++ assets/js/swiper.js
@@ -6,22 +6,51 @@ var flkty = new Flickity(document.querySelector('.main-gallery'), {
 });
 
 var swiper = new Swiper(".best-seller", {
+    loop: true,
     navigation: {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
     },
 });
 
-var swiper = new Swiper(".swiper2", {
+var swiper = new Swiper(".recomended-swiper", {
+    loop: true,
+    breakpoints: {
+        "@0.00": {
+          slidesPerView: 1,
+        },
+        "@0.75": {
+          slidesPerView: 2,
+        },
+        "@1.00": {
+          slidesPerView: 3,
+        },
+        "@1.50": {
+          slidesPerView: 4,
+        },
+    },
     navigation: {
         nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
     },
 });
 
-var swiper = new Swiper(".swiper3", {
+var swiper = new Swiper(".popular-swiper", {
+    loop: true,
+    breakpoints: {
+        "@0.00": {
+          slidesPerView: 1,
+        },
+        "@0.75": {
+          slidesPerView: 2,
+        },
+        "@1.00": {
+          slidesPerView: 3,
+        },
+        "@1.50": {
+          slidesPerView: 4,
+        },
+    },
     navigation: {
         nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
     },
 });
